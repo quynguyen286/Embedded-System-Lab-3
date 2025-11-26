e4cb61603c6cbd21c23d260bdacb59afcbfa0b08
QUY\QUÝ <quy.nguyencord@hcmut.edu.vn>
2025-11-26 10:04:11 +0700

    v1.1

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "global.h"
+
+#include "led_blinky.h"
+#include "neo_blinky.h"
+#include "temp_humi_monitor.h"
+#include "mainserver.h"
+#include "tinyml.h"
+#include "coreiot.h"
+
+// include task
+#include "task_check_info.h"
+#include "task_toogle_boot.h"
+#include "task_wifi.h"
+#include "task_webserver.h"
+#include "task_core_iot.h"
+
+void setup()
+{
+  Serial.begin(115200);
+
+  xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
+  xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
+  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
+  xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
+  xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
+}
+
+void loop()
+{
+
+}
